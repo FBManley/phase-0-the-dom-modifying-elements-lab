04017649ef3fec68091f19240ce990dba7cce8bf
Jacob Manley <jbmanley45@gmail.com>
2022-03-23 17:29:44 -0700

    commit tree ef2889e275ce44075c5cd6640dc5b5e970da2481

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const element = document.getElementById("main")
+element.remove();
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
+// var element = document.querySelector('main#main').remove
+//document.querySelector('main')
 // Write your code here!
